5f5507d0b3a76fb0897292e29effa61543bd6ac0
James Cho <hwisoocho@gmail.com>
2019-05-20 21:18:03 -0700

    break down lat and lng into variables

--- geolocation.py
+++ geolocation.py
@@ -7,9 +7,18 @@ import json
 
 try:
     data = json.load(request.urlopen('http://ipinfo.io/json'))
+    latlong = data['loc'].split(',')
+    lat = latlong[0]
+    long = latlong[1]
+
     print(data)
 except Exception as e:
     print(e)
 else:
     print('You are near: {city}, {region}, {postal}, {country}'.format(**data))
-    print('        Lat/Lng: {}E'.format(data['loc'].replace(',', 'N, ')))
+    print('Lat: {}'.format(lat))
+    print('Lng: {}'.format(long))
+
+
+
+
