6e82801f5843bcf579aa96f3f76939072d274bc2
James Cho <hwisoocho@gmail.com>
2019-05-20 21:36:28 -0700

    convert lat and long to floats, create folium map and save to html

--- geolocation.py
+++ geolocation.py
@@ -4,21 +4,25 @@
 
 from urllib import request
 import json
+import folium
 
 try:
     data = json.load(request.urlopen('http://ipinfo.io/json'))
-    latlong = data['loc'].split(',')
-    lat = latlong[0]
-    long = latlong[1]
+
 
     print(data)
 except Exception as e:
     print(e)
 else:
+    latlong = data['loc'].split(',')
+    lat = float(latlong[0])
+    long = float(latlong[1])
     print('You are near: {city}, {region}, {postal}, {country}'.format(**data))
+
     print('Lat: {}'.format(lat))
     print('Lng: {}'.format(long))
 
 
-
+map = folium.Map(location=[lat, long], zoom_start=6, tiles='Stamen Terrain')
+map.save('location.html')
 
